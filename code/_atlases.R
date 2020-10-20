b985bce96743e6e0c8bea18cbc00ecb664a4f581
mcfreund <m.freund@wustl.edu>
2020-10-20 17:10:59 -0500

    first commit

---
+++ code/_atlases.R
@@ -0,0 +1,36 @@
+
+## atlases ----
+
+hcp <- list(
+  L  = readGIfTI(
+    file.path(dir.atlas, "surf", "HCP_S1200_GroupAvg_v1", "S1200.L.midthickness_MSMAll.32k_fs_LR.surf.gii")
+  ),
+  R = readGIfTI(
+    file.path(dir.atlas, "surf", "HCP_S1200_GroupAvg_v1", "S1200.R.midthickness_MSMAll.32k_fs_LR.surf.gii")
+  )
+)
+
+parcellation <- mikeutils::read_atlas("schaefer400")
+
+schaefer10k <-
+  c(
+    gifti::read_gifti(file.path(dir.schaefer, "Schaefer2018_400Parcels_7Networks_order_10K_L.label.gii"))$data[[1]],
+    gifti::read_gifti(file.path(dir.schaefer, "Schaefer2018_400Parcels_7Networks_order_10K_R.label.gii"))$data[[1]] + 200
+  )
+
+
+over <- list(
+  L = parcellation$atlas[1:(nrow(parcellation$atlas) / 2)], 
+  R = parcellation$atlas[(nrow(parcellation$atlas) / 2):nrow(parcellation$atlas)]
+)
+
+schaefer <- list(
+  L = read_gifti2matrix(
+    file.path(dir.schaefer, "Schaefer2018_400Parcels_7Networks_order_L.label.gii")
+  ) %>% c,
+  R = read_gifti2matrix(
+    file.path(dir.schaefer, "Schaefer2018_400Parcels_7Networks_order_R.label.gii")
+  ) %>% c
+)
+
+
---
+++ code/_vars.R
@@ -0,0 +1,55 @@
+nodename <- Sys.info()["nodename"]
+
+dir.nil.dmcc2.afni <- "/data/nil-bluearc/ccp-hcp/DMCC_ALL_BACKUPS/HCP_SUBJECTS_BACKUPS/AFNI_ANALYSIS"
+
+nodename <- Sys.info()["nodename"]
+
+if (nodename == "ccplinux1") {
+  
+  dir.atlas <- "/data/nil-external/ccp/freund/atlases"
+  dir.schaefer <- "/data/nil-bluearc/ccp-hcp/DMCC_ALL_BACKUPS/ATLASES/"
+  
+} else if (nodename == "CCP-FREUND") {
+  ## mike freund's (i.e., ccp's) thinkpad
+  ## reliant on box drive
+  ## assumes box drive location at ./Users/mcf/Box
+  
+  dir.atlas <- "C:/local/atlases"
+  dir.schaefer <- dir.atlas
+  
+}
+
+n.cores <- detectCores()
+
+
+tasks <- c("Axcpt", "Cuedts", "Stern", "Stroop")
+dir.analysis <- here("out", "glms")
+
+n.trs <- c(
+  Axcpt   = 1220,
+  # Axcpt_proactive  = 1220,
+  # Axcpt_reactive   = 1220,
+  Cuedts  = 1300,
+  # Cuedts_proactive = 1300,
+  # Cuedts_reactive  = 1300,
+  Stern   = 1200,
+  # Stern_proactive  = 1200,
+  # Stern_reactive   = 1200,
+  Stroop  = 1080
+  # Stroop_proactive = 1080,
+  # Stroop_reactive  = 1180
+)
+
+dmcc34 <- c(
+  22, 77, 78, 86, 87, 91, 93, 99, 101, 103, 105, 107, 110, 127, 130, 139, 140,
+  144, 148, 172, 175, 185, 189, 219, 301, 303, 306, 314, 340, 346, 347, 349, 350, 353
+)
+
+target.trs <- list(
+  Axcpt = 7:12,
+  Cuedts = 7:11,
+  Stern = 11:16,
+  Stroop = 2:6
+)
+
+
